495c1cce2b3db24d44d409a1abd3480bf81ff876
Sangmin <88222336+CaoCao-99@users.noreply.github.com>
2023-07-11 22:12:26 +0900

    10942(팰린드롬?)

--- 10942.py
+++ 10942.py
@@ -1,21 +1,39 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 data = list(map(int,input().split()))
 m = int(input())
+dp = [[-1 for j in range(n)]for i in range(n)]
+for i in range(n-1):#(0,1) ~ (n-2,n-1)
+    if data[i] == data[i+1]:
+        dp[i][i+1] = 1
+    else:
+        dp[i][i+1] = 0
+for i in range(n-2):
+    if data[i] == data[i+2]:
+        dp[i][i+2] = 1
+    else:
+        dp[i][i+2] = 0
+
+for i in range(n-3,-1,-1):
+    diff = n-i
+    for j in range(0,i):
+        if dp[j+1][j+diff-1] == 1 and data[j] == data[j+diff]:
+            dp[j][j+diff] = 1
+        else:
+            dp[j][j+diff] = 0
+
+
+
 slicing_data = []
 for i in range(m):
     a,b = map(int,input().split())
-    slicing_data.append((a,b))
+    slicing_data.append((a-1,b-1))
 
 for a,b in slicing_data:
     if a == b:
         print(1)
-    elif (b-a)%2 == 0:
-        if data[a-1:(a+b)//2-1] == data[(a+b)//2+1:b][::-1]:
-            print(1)
-        else:
-            print(0)
     else:
-        if data[a-1:(a+b)//2+1] == data[(a+b)//2+1:b][::-1]:
-            print(1)
-        else:
-            print(0)
+        print(dp[a][b])
+        
