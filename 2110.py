425f5ee25986c64956caf42410f9a56728f9b27b
Sangmin Lee <sm32289@naver.com>
2023-07-26 13:21:29 +0900

    2110(공유기 설치하기)

--- 2110.py
+++ 2110.py
@@ -4,8 +4,6 @@ data = sorted(data)
 dist = []
 for i in range(len(data) - 1):
     dist.append(data[i+1] - data[i])
-    
-print(dist)
 low = min(dist)
 high = sum(dist)
 mid = (low + high)//2
@@ -14,21 +12,19 @@ while low <= high:
     mid = (low+high)//2
     cnt = 0
     cntcnt = 0
-    answer_a = high
     low_check = False
     for i in range(len(dist)):
         cnt += dist[i]
-        if cnt > mid:
-            cnt = dist[i]
+        if cnt >= mid:
+            cnt = 0
             cntcnt+=1
-            answer_a = min(answer_a, cnt - dist[i])
         if cntcnt >= k-1:
             low_check = True
             break
     if low_check:
-        answer = answer_a
+        answer = mid
         low = mid + 1
     else:
         high = mid - 1
 
-print(high)
+print(answer)
