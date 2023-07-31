8979c9a1f8a0e029754a9fdb3185a27d0ee27ebd
Sangmin <88222336+CaoCao-99@users.noreply.github.com>
2023-07-31 23:37:13 +0900

    4781(사탕 가게)

---
+++ 4781.py
@@ -0,0 +1,26 @@
+from collections import deque
+answer = []
+while True:
+    n, budget = map(float,input().split())
+    n = int(n)
+    if n == 0 and budget == 0:
+        break
+    candy = [list(map(float,input().split()))for i in range(n)]
+    visit = dict()
+    q = deque()
+    q.append(0)
+    visit[0] = 0
+    while q:
+        now = q.popleft()
+        for i,j in candy:
+            if now + j <= budget:
+                if not (now + j) in visit:
+                    visit[now+j] = visit[now] + i
+                    q.append(now + j)
+                else:
+                    if visit[now+j] < visit[now] + i:
+                        visit[now+j] = visit[now] + i
+                        #q.append(now+j)
+    answer.append(max(visit.values()))
+for i in answer:
+    print(i)
