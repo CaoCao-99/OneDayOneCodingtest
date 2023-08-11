809edfb8224ea8be3bdfbdb1696ecfb77932806d
Sangmin <88222336+CaoCao-99@users.noreply.github.com>
2023-08-11 15:16:03 +0900

    17825(주사위 윷놀이)

---
+++ 17825.py
@@ -0,0 +1,31 @@
+import copy
+data = list(map(int, input().split()))
+normal_map = range(0,41,2)
+special_map1 = [10,13,16,19,25,30,35,40]
+special_map2 = [20,22,24,25,30,35,40]
+special_map3 = [30,28,27,26,25,30,35,40]
+horse = [[normal_map, 0],[normal_map, 0],[normal_map, 0],[normal_map, 0]] #말이 속한 맵, 말이 속한 맵안에서의 위치(인덱스)
+answer = 0
+def rec(horse, cnt, ans): #현재 말이 어떤 map에 있는지 표현, 현재 진행한 횟수, 현재까지 오는데 얻은 포인트
+    if cnt == 10:
+        answer = max(answer, ans)
+        return
+    for i in range(4):
+        new_horse = copy.deepcopy(horse)
+        if new_horse[i][1] < len(new_horse[i][0]):
+            if new_horse[i][0][new_horse[i][1]] == 10:
+                new_horse[i][0] = special_map1
+                new_horse[i][1] = data[cnt]
+                rec(new_horse, cnt + 1, ans + special_map1[data[cnt]])
+                
+            elif new_horse[i][0][new_horse[i][1]] == 20:
+                new_horse[i][0] = special_map2
+                new_horse[i][1] = data[cnt]
+                rec(new_horse, cnt + 1, ans + special_map2[data[cnt]])
+                
+            elif new_horse[i][0][new_horse[i][1]] == 30:
+                new_horse[i][0] = special_map3
+                new_horse[i][1] = data[cnt]
+                rec(new_horse, cnt + 1, ans + special_map3[data[cnt]])
+                
+            ans = rec(cnt + 1, ans + )
