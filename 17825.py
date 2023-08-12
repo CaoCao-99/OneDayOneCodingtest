283f8106e294f6e0fb6955b5353b3d7f31dde206
Sangmin <88222336+CaoCao-99@users.noreply.github.com>
2023-08-12 17:34:53 +0900

    17825(주사위 윷놀이)

--- 17825.py
+++ 17825.py
@@ -1,9 +1,10 @@
 import copy
 data = list(map(int, input().split()))
-normal_map = range(0,41,2)
-special_map1 = [10,13,16,19,25,30,35,40]
-special_map2 = [20,22,24,25,30,35,40]
-special_map3 = [30,28,27,26,25,30,35,40]
+normal_map = list(range(0,41,2))
+normal_map.append(0)
+special_map1 = [10,13,16,19,25,30,35,40,0]
+special_map2 = [20,22,24,25,30,35,40,0]
+special_map3 = [30,28,27,26,25,30,35,40,0]
 horse = [[0, 0],[0, 0],[0, 0],[0, 0]] #말이 속한 맵, 말이 속한 맵안에서의 위치(인덱스)
 visit = dict()
 visit['00000000'] = True
@@ -26,7 +27,6 @@ def get_func(num):
 def rec(horse, cnt, ans, visit): #현재 말이 어떤 map에 있는지 표현, 현재 진행한 횟수, 현재까지 오는데 얻은 포인트
     global answer
     if cnt == 10:
-        print(visit)
         answer = max(answer, ans)
         return
     
@@ -34,15 +34,15 @@ def rec(horse, cnt, ans, visit): #현재 말이 어떤 map에 있는지 표현, 
         new_horse = copy.deepcopy(horse)
         now_map = get_func(new_horse[i][0])
         if new_horse[i][1] < len(now_map) - 1:
-            if now_map[new_horse[i][1]] == 10:
+            if now_map[new_horse[i][1]] == 10 and new_horse[i][0] == 0:
                 new_horse[i][0] = 1
                 new_horse[i][1] = data[cnt]
                 v=''
-                aa = set()
+                aa = []
                 for j in range(4):
                     v += str(new_horse[j][0]) + str(new_horse[j][1])
                     if i != j:
-                        aa.add(str(new_horse[j][0]) + str(new_horse[j][1]))
+                        aa.append(str(new_horse[j][0]) + str(new_horse[j][1]))
                     
                 if v in visit or str(new_horse[i][0]) + str(new_horse[i][1]) in aa:
                     continue
@@ -51,15 +51,15 @@ def rec(horse, cnt, ans, visit): #현재 말이 어떤 map에 있는지 표현, 
                     rec(new_horse, cnt + 1, ans + special_map1[new_horse[i][1]], visit)
                     del visit[v]
                 
-            elif now_map[new_horse[i][1]] == 20:
+            elif now_map[new_horse[i][1]] == 20 and new_horse[i][0] == 0:
                 new_horse[i][0] = 2
                 new_horse[i][1] = data[cnt]
                 v=''
-                aa = set()
+                aa = []
                 for j in range(4):
                     v += str(new_horse[j][0]) + str(new_horse[j][1])
                     if i != j:
-                        aa.add(str(new_horse[j][0]) + str(new_horse[j][1]))
+                        aa.append(str(new_horse[j][0]) + str(new_horse[j][1]))
                     
                 if v in visit or str(new_horse[i][0]) + str(new_horse[i][1]) in aa:
                     continue
@@ -69,15 +69,15 @@ def rec(horse, cnt, ans, visit): #현재 말이 어떤 map에 있는지 표현, 
                     del visit[v]
 
                 
-            elif now_map[new_horse[i][1]] == 30:
+            elif now_map[new_horse[i][1]] == 30 and new_horse[i][0] == 0:
                 new_horse[i][0] = 3
                 new_horse[i][1] = data[cnt]
                 v=''
-                aa = set()
+                aa = []
                 for j in range(4):
                     v += str(new_horse[j][0]) + str(new_horse[j][1])
                     if i != j:
-                        aa.add(str(new_horse[j][0]) + str(new_horse[j][1]))
+                        aa.append(str(new_horse[j][0]) + str(new_horse[j][1]))
                     
                 if v in visit or str(new_horse[i][0]) + str(new_horse[i][1]) in aa:
                     continue
@@ -90,13 +90,13 @@ def rec(horse, cnt, ans, visit): #현재 말이 어떤 map에 있는지 표현, 
                 next = min(len(now_map) - 1,new_horse[i][1] + data[cnt])
                 new_horse[i][1] = next
                 v=''
-                aa = set()
+                aa = []
                 for j in range(4):
                     v += str(new_horse[j][0]) + str(new_horse[j][1])
                     if i != j:
-                        aa.add(str(new_horse[j][0]) + str(new_horse[j][1]))
+                        aa.append(str(new_horse[j][0]) + str(new_horse[j][1]))
                     
-                if v in visit or str(new_horse[i][0]) + str(new_horse[i][1]) in aa:
+                if v in visit or (now_map[next] != 0 and str(new_horse[i][0]) + str(new_horse[i][1]) in aa):
                     continue
                 else:
                     visit[v] = True
--- 17825_3.py
+++ 17825_3.py
@@ -3,6 +3,7 @@ data = list(map(int, input().split()))
 normal_map = list(range(0,41,2))
 normal_map.append(0)
 print(normal_map)
+print(len(normal_map))
 special_map1 = [10,13,16,19,25,30,35,40,0]
 special_map2 = [20,22,24,25,30,35,40,0]
 special_map3 = [30,28,27,26,25,30,35,40,0]
@@ -28,42 +29,84 @@ def rec(horse, cnt, ans): #현재 말이 어떤 map에 있는지 표현, 현재 
     if cnt == 10:
         answer = max(answer, ans)
         return
+    new_horse = copy.deepcopy(horse)
     for i in range(4): 
-        new_horse = copy.deepcopy(horse)
         now_map = get_func(horse[i][0])
         if new_horse[i][1] < len(now_map) - 1:
             if now_map[new_horse[i][1]] == 10 and new_horse[i][0] == 0:
+                b_i = 0
+                b_j = new_horse[i][1]
                 new_horse[i][0] = 1
                 new_horse[i][1] = data[cnt]
                 if [new_horse[i][0], new_horse[i][1]] in horse:
+                    new_horse[i] = [b_i,b_j]
                     continue
+                if new_horse[i][1] >= 4:
+                    if [2,new_horse[i][1]-1] in horse or [3,new_horse[i][1]] in horse:
+                        new_horse[i] = [b_i,b_j]
+                        continue
                 else:
                     rec(new_horse, cnt + 1, ans + special_map1[new_horse[i][1]])
+                    new_horse[i] = [b_i,b_j]
 
             elif now_map[new_horse[i][1]] == 20 and new_horse[i][0] == 0:
+                b_i = 0
+                b_j = new_horse[i][1]
                 new_horse[i][0] = 2
                 new_horse[i][1] = data[cnt]
                 if [new_horse[i][0], new_horse[i][1]] in horse:
+                    new_horse[i] = [b_i,b_j]
                     continue
+                if new_horse[i][1] >= 3:
+                    if [1,new_horse[i][1]+1] in horse or [3,new_horse[i][1] + 1] in horse:
+                        new_horse[i] = [b_i,b_j]
+                        continue
                 else:
                     rec(new_horse, cnt + 1, ans + special_map2[new_horse[i][1]])
+                    new_horse[i] = [b_i,b_j]
  
             elif now_map[new_horse[i][1]] == 30 and new_horse[i][0] == 0:
+                b_i = 0
+                b_j = new_horse[i][1]
                 new_horse[i][0] = 3
                 new_horse[i][1] = data[cnt]
                 if [new_horse[i][0], new_horse[i][1]] in horse:
+                    new_horse[i] = [b_i,b_j]
                     continue
+                if new_horse[i][1] >= 4:
+                    if [2,new_horse[i][1]-1] in horse or [1,new_horse[i][1]] in horse:
+                        new_horse[i] = [b_i,b_j]
+                        continue
                 else:
                     rec(new_horse, cnt + 1, ans + special_map3[new_horse[i][1]])
-
+                    new_horse[i] = [b_i,b_j]
             
             else:
                 next = min(len(now_map) - 1,new_horse[i][1] + data[cnt])
+                b_i = new_horse[i][0]
+                b_j = new_horse[i][1]
                 new_horse[i][1] = next
                 if [new_horse[i][0], new_horse[i][1]] in horse and next != len(now_map) - 1:
+                    new_horse[i] = [b_i,b_j]
                     continue
+                if new_horse[i][0] == 1:
+                    if new_horse[i][1] >= 4 and new_horse[i][1] != len(now_map) - 1:
+                        if [2,new_horse[i][1]-1] in horse or [3,new_horse[i][1]] in horse:
+                            new_horse[i] = [b_i,b_j]
+                            continue
+                if new_horse[i][0] == 2:
+                    if new_horse[i][1] >= 3 and new_horse[i][1] != len(now_map) - 1:
+                        if [1,new_horse[i][1]+1] in horse or [3,new_horse[i][1] + 1] in horse:
+                            new_horse[i] = [b_i,b_j]
+                            continue
+                if new_horse[i][0] == 3:
+                    if new_horse[i][1] >= 4 and new_horse[i][1] != len(now_map) - 1:
+                        if [2,new_horse[i][1]-1] in horse or [1,new_horse[i][1]] in horse:
+                            new_horse[i] = [b_i,b_j]
+                            continue
                 else:
                     rec(new_horse, cnt + 1, ans + now_map[next])
+                    new_horse[i] = [b_i,b_j]
 
 
 
