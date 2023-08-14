f57cc288785460e1b1f296a83474b88eb436d6b3
CaoCao-99 <88222336+CaoCao-99@users.noreply.github.com>
2023-08-14 18:33:06 +0900

    2494(숫자 맞추기)

--- 2494.py
+++ 2494.py
@@ -1,3 +1,16 @@
 n = int(input())
-now = int(input())
+start = int(input())
 want = int(input())
+answer = 0
+
+def change(cnt, ans, now):
+    global answer
+    if cnt > answer:
+        return
+    if ans == str(want) or now == len(str(want)) - 1:
+        answer = min(answer, cnt)
+        return
+    plus = (want[cnt] - int(ans[cnt]))%10
+    change(cnt + 1, )
+
+    
