d3771a930c69d0ccce714e6e9a56c05b3d6562d6
CaoCao-99 <88222336+CaoCao-99@users.noreply.github.com>
2023-08-23 23:58:14 +0900

    1208(부분수열의 합2)

--- 1208_2.py
+++ 1208_2.py
@@ -5,33 +5,45 @@ data = list(map(int,input().split()))
 data = sorted(data)
 left = []
 right = []
-l_cnt = []
-r_cnt = []
+real_left = []
+real_right = dict()
+
 for i in range(n):
     if i < n//2:
         left.append(data[i])
     else:
         right.append(data[i])
+        if data[i] not in real_right:
+            real_right[data[i]] = 1
+        else:
+            real_right[data[i]] += 1
 
 real_left = copy.deepcopy(left)
-
-real_right = copy.deepcopy(right)
 
 for i in range(2,len(left)+1):
     a = combinations(left, i)
     for j in a:
-        real_left.append(sum(j))
+        now = sum(j)
+        real_left.append(now)
+
 
 for i in range(2,len(right)+1):
     a = combinations(right, i)
     for j in a:
-        real_right.append(sum(j))
-
+        now = sum(j)
+        if now not in real_right:
+            real_right[now] = 1
+        else:
+            real_right[now] += 1
 answer = 0
-real_left = sorted(real_left)
-real_right = sorted(real_right)
 answer += real_left.count(s)
-answer += real_right.count(s)
+if s in real_right:
+    answer += real_right[s]
+
+real_left = sorted(real_left)
+real_right = sorted(real_right.items())
+
+
 for i in real_left:
     l = 0
     r = len(real_right)-1
@@ -40,12 +52,12 @@ for i in real_left:
         mid = (l+r)//2
         if mid < 0 or mid >= len(real_right):
             break
-        if s < real_right[mid] + i:
+        if s < real_right[mid][0] + i:
             r = mid - 1
-        if s > real_right[mid] + i:
+        if s > real_right[mid][0] + i:
             l = mid + 1
-        if s == real_right[mid] + i:
-            answer += real_right.count(real_right[mid])
+        if s == real_right[mid][0] + i:
+            answer += real_right[mid][1]
             break
 
 print(answer)       
