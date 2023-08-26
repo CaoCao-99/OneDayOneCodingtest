b044564fa3cc562b81f00b53be401d1652ef02f9
CaoCao-99 <88222336+CaoCao-99@users.noreply.github.com>
2023-08-26 23:58:39 +0900

    2251(물통)

---
+++ 2251.py
@@ -0,0 +1,74 @@
+from collections import deque
+a,b,c = map(int,input().split())
+q = deque()
+d = dict()
+q.append([0,0,c])
+d[str(0) + str(0) + str(c)] = 1
+answer = set()
+
+def find(q,w,e):
+    send = []
+    if q < a:
+        send.append(0)
+    if w < b:
+        send.append(1)
+    if e < c:
+        send.append(2)
+    return send
+
+
+while q:
+    n_a,n_b,n_c = q.popleft()
+    if n_a == 0 and n_c >0:
+        answer.add(n_c)
+    can_put = find(n_a,n_b,n_c)
+    for i in can_put:
+        if i == 0:
+            if n_b > 0:
+                if n_b > a - n_a and str(a) +str(n_b - a + n_a) + str(n_c) not in d:
+                    d[str(a) +str(n_b - a + n_a) + str(n_c)] = 1
+                    q.append([a,n_b - a + n_a,n_c])
+                elif n_b <= a - n_a and str(n_b + n_a) + str(0) + str(n_c) not in d:
+                    d[str(n_b + n_a) + str(0) + str(n_c)] = 1
+                    q.append([n_b + n_a,0,n_c])
+            if n_c > 0:
+                if n_c >  a - n_a and str(a) + str(n_b) + str(n_c - a + n_a) not in d:
+                    d[str(a) + str(n_b) + str(n_c - a + n_a)] = 1
+                    q.append([a,n_b,n_c - a + n_a])
+                elif n_c <=  a - n_a and str(n_c + n_a) + str(n_b) + str(0) not in d:
+                    d[str(n_c + n_a) + str(n_b) + str(0)] = 1
+                    q.append([n_c + n_a, n_b ,0])                
+        if i == 1:
+            if n_a > 0:
+                if n_a > b - n_b and str(n_a - b + n_b) + str(b) + str(n_c) not in d:
+                    d[str(n_a - b + n_b) + str(b) + str(n_c)] = 1
+                    q.append([n_a - b + n_b, b, n_c])
+                elif n_a <= b - n_b and str(0) + str(n_a + n_b) + str(n_c) not in d:
+                    d[str(0) + str(n_a + n_b) + str(n_c)] = 1
+                    q.append([0 ,n_a + n_b,n_c])
+            if n_c > 0:
+                if n_c > b - n_b and str(n_a) + str(b) + str(n_c - b + n_b) not in d:
+                    d[str(n_a) + str(b) + str(n_c - b + n_b)] = 1
+                    q.append([n_a , b, n_c - b + n_b])
+                elif n_c <=  b - n_b and str(n_a) + str(n_b + n_c) + str(0) not in d:
+                    d[str(n_a) + str(n_b + n_c) + str(0)] = 1
+                    q.append([n_a, n_b + n_c ,0])  
+        if i == 2:
+            if n_a > 0:
+                if n_a > c - n_c and str(n_a - c + n_c) + str(n_b) +  str(c) not in d:
+                    d[str(n_a - c + n_c) + str(n_b) +  str(c)] = 1
+                    q.append([n_a - c + n_c, n_b, c])
+                elif n_a <= c - n_c and str(0) + str(n_b) + str(n_c + n_a) not in d:
+                    d[str(0) + str(n_b) + str(n_c + n_a)] = 1
+                    q.append([0, n_b, n_c + n_a])
+            if n_b > 0:
+                if n_b >  c - n_c and str(n_a) + str(n_b - c + n_c) + str(c) not in d:
+                    d[str(n_a) + str(n_b - c + n_c) + str(c)] = 1
+                    q.append([n_a, n_b - c + n_c, c])
+                elif n_b <=  c - n_c and str(n_a) +  str(0) + str(n_b + n_c) not in d:
+                    d[str(n_a) +  str(0) + str(n_b + n_c)] = 1
+                    q.append([n_a, 0 , n_b + n_c])  
+
+answer = sorted(answer)
+for i in answer:
+    print(i, end = ' ')
