2c75e0f9c201c3c1de4ae887e764756babd830eb
Sangmin <88222336+CaoCao-99@users.noreply.github.com>
2023-09-04 17:34:24 +0900

    2662(기업 투자)

--- 2662.py
+++ 2662.py
@@ -1,17 +1,22 @@
 import copy
 n, m = map(int,input().split())
 data = [[0 for _ in range(m+1)]] + [list(map(int,input().split())) for _ in range(n)]
-print(data)
-dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
-answer = [0] * (n+1)
+dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+answer = [[[0 for _ in range(m+1)] for _ in range(m+1)] for _ in range(n+1)]
 
-for i in range(1,n+1):
-    for j in range(1,m+1):
-        for dev in range(i+1):
-            dp[j][i] = max(data[i][j])
+for i in range(1,n+1):#돈
+    for j in range(1,m+1):#회사
+        dp[j][i] = max(data[i][j], dp[j-1][i])
+        if dp[j][i] == data[i][j]:
+            answer[i][j][j] = i
+        else:
+            answer[i][j] = copy.deepcopy(answer[i][j-1])
+        for dev in range(1, i+1):#분할 투자
+            dp[j][i] = max(data[i-dev][j] + dp[j-1][dev], dp[j][i])
+            if dp[j][i] == data[i-dev][j] + dp[j-1][dev]:
+                answer[i][j] = copy.deepcopy(answer[dev][j-1])
+                answer[i][j][j] = i - dev
 
 
-
-#
-
-#dp[i][j] = max(dp[i-1][j-weight] + value, dp[i-1][j])
+print(dp[m][n])
+print(*answer[n][m][1:])
