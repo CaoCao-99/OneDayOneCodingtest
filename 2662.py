efc542d6fee35b13ec56faddb27c48826e2880d7
CaoCao-99 <88222336+CaoCao-99@users.noreply.github.com>
2023-09-04 12:51:02 +0900

    2662(기업 투자)

--- 2662.py
+++ 2662.py
@@ -5,10 +5,10 @@ print(data)
 dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
 answer = [0] * (n+1)
 
-for a in range(1,n+1):#현재 회사 번호
-    for b in range(1,a+1):
-        for c in range(1, m+1):# 현재 회사에 쓸 금액
-            dp[a][c] = max(dp[])
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        for dev in range(i+1):
+            dp[j][i] = max(data[i][j])
 
 
 
